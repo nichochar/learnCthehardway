0c95ffacc5bcc89968a45699b35b3ca4c6086061
Nicholas Charriere <nicholascharriere@gmail.com>
2013-12-13 23:37:08 +0100

    Added first exercises and makefile

---
+++ Makefile
@@ -0,0 +1,15 @@
+CFLAGS=-Wall -g
+
+all: ex1 ex3 ex5 ex6 ex7
+
+clean:
+	rm -f ex1
+	rm -f -r ex1.dSYM
+	rm -f ex3
+	rm -f -r ex3.dSYM
+	rm -f ex5
+	rm -f -r ex5.dSYM
+	rm -f ex6
+	rm -f -r ex6.dSYM
+	rm -f ex7
+	rm -f -r ex7.dSYM
---
+++ ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int age = 10;
+  int height = 72;
+
+  printf("I am %d years old.\n", age);
+  printf("Newline \n newline tab \t tab vertab \v vertab new page \f new page backspace \b backspace carriage return \r carriage return\r");
+  printf("I am %d inches tall.\n", height);
+
+  return 0;
+}
