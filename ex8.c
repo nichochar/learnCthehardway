736d91c8f1ab9a03ca4376797748691699f92db0
Nicholas Charriere <nicholascharriere@gmail.com>
2013-12-14 11:55:10 +0100

    Added small change to ex8 with new assignment

--- ex8.c
+++ ex8.c
@@ -3,12 +3,13 @@
 int main(int argc, char *argv[])
 {
   int areas[] = {10, 12, 13, 14, 20};
-  char name[] = "Zed";
   char full_name[] = {
     'Z', 'e', 'd',
     ' ', 'A', '.', ' ',
-    'S', 'h', 'a', 'w', '\0'
+    'S', 'h', 'a', 'w','\0'
   };
+  char name[] = "Zed";
+  areas[0] = name[1];
 
   // Warning: On some systems you may have to change the 
   // %ld in this code to a %u since it will use unsigned ints
